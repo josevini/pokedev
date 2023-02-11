d7d2a97aac2568659a2a533d5df27bbc3b6df567
Vinicius Silva <ferreiravini468@gmail.com>
2023-02-11 11:37:27 -0300

    queries optimized and css fix

--- src/components/SearchPokemonItem.vue
+++ src/components/SearchPokemonItem.vue
@@ -10,30 +10,27 @@
     function getChain(chain, evolutions = []) {
 
         evolutions.push(chain.species.name)
-        
+
         if (chain.evolves_to.length) {
             getChain(chain.evolves_to[0], evolutions)
         }
-        
+
         return evolutions
     }
     
 </script>
 
 <template>
-    <section>
+    <section class="form-container">
         <input v-model="search" class="form-search" />
-        <button @click="getPokemonByName(search).then(response => {
-
-            // searchData.setPokemonId(response.data.id)
-            // searchData.setPokemonName(response.data.name)
-            // searchData.setPokemonStats(response.data.stats)
-
-            getPokemonSpecies(search).then(response => {
+        <button class="btn-search" @click="getPokemonSpecies(search).then(response => {
 
             getPokemonEvolutionChains(response.data.evolution_chain.url).then(response => {
+
+                allEvolutions = []
                 
-                getChain(response.data.chain).forEach((value) => {
+                getChain(response.data.chain)
+                .forEach((value) => {
 
                     getPokemonByName(value).then(response => {
                         allEvolutions.push(response.data)
@@ -43,24 +40,34 @@
                 searchData.setPokemonEvolutions(allEvolutions)
                 
             })
-        })
         })">Pesquisar</button>
-        {{ allEvolutions.length }}
     </section>
 </template>
 <style scoped>
     .form-container {
-
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     .form-search {
+        width: calc(50% - 10px);
+        height: 30px;
         outline: none;
         border: 1px solid rgb(216, 216, 216);
-        border-radius: 20px;
-        padding: 5px;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+        padding: 7px;
     }
 
     .btn-search {
-        
+        background-color: #FFDC7D;
+        height: 30px;
+        padding: 7px;
+        border: none;
+        color: #a08334;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
     }
 </style>
