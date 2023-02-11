3b761c0dd0219cbf6e277000274f5c6618891f32
Vinicius Silva <ferreiravini468@gmail.com>
2023-02-10 21:58:51 -0300

    HomeView created

---
+++ src/view/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import SearchPokemonItem from '../components/SearchPokemonItem.vue'
+  import ShowPokemonItem from '../components/ShowPokemonItem.vue'
+</script>
+
+<template>
+    <SearchPokemonItem />
+    <ShowPokemonItem />
+</template>
